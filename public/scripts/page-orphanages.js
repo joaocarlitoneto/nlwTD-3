a3ef710ec58e515f13be7181c59b9bab9825be28
Joao <joaocarlitoneto@gmail.com>
2020-10-15 18:02:34 -0300

    Corringo bug em orphanages.js

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -19,7 +19,7 @@ function addMarker({id, name, lat, lng}){
     className: "map-popup",
     minWidth: 240,
     minHeight: 240
-  }).setContent(`${name} <a href="/orphanage?id=${id}" <img src="/images/arrow-white.svg"> </a>`)
+  }).setContent(`${name} <a href="/orphanage?id=${id}"><img src="/images/arrow-white.svg"> </a>`)
 
   // create and add marker
   L.marker([lat,lng], { icon })
