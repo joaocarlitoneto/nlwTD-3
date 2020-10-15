1e2d5598fcb3d10f5c27379f47def69f377db900
Joao <joaocarlitoneto@gmail.com>
2020-10-15 14:50:04 -0300

    instalando nodemon

---
+++ src/server.js
@@ -0,0 +1,13 @@
+// importar dependência
+const express = require('express');
+
+// iniciando lib express
+const server = express()
+
+// criar uma rota
+server.get('/', () => {
+  
+})
+
+// ligar o servidor
+server.listen(5500)
