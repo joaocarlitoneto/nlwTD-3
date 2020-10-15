cef2bbe300a9c34b8c4cb66fb1e1757c40651454
Joao <joaocarlitoneto@gmail.com>
2020-10-15 17:12:58 -0300

    Finalizando parte de backend - Aula 04

---
+++ src/database/fakedata.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-25.4738143",
+    lng: "-49.2600019",
+    name: "Lar das meninas",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1562790519-bc8a4a47cd0e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/photo-1600711725407-2ea4733a38c3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas das 8h até as 18h",
+    open_on_weekends: "1"
+
+  },
+  {
+    id: 2,
+    lat: "-25.4765143",
+    lng: "-49.2629019",
+    name: "Casa dos meninos",
+    description: "Presta assistência a crianças de 02 a 10 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1562790519-bc8a4a47cd0e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/photo-1600711725407-2ea4733a38c3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas das 10h até as 20h",
+    open_on_weekends: "0"
+  }
+]
