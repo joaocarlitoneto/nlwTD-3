deda1aebe5f417f5de055df14cccfbf66aacbb18
Joao <joaocarlitoneto@gmail.com>
2020-10-15 15:13:50 -0300

    adicionando o hbs handlebars

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 // importar dependência
 const express = require('express');
+const path = require('path');
 
 // iniciando lib express
 const server = express()
 
+server
+// utilizando os arquivos estáticos
+.use(express.static('public'))
+
 // criar uma rota
-server.get('/', () => {
-  
+.get('/', (request, response) => {
+  return response.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 // ligar o servidor
